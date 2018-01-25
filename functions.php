a2e75b8ba5ca221472174246a16bbc3109adb183
Bruno Monteiro <brunomonteiro3@gmail.com>
2018-01-25 18:50:21 -0200

    Enabling seoURL function.

--- functions.php
+++ functions.php
@@ -1,8 +1,12 @@
 <?php
+	/* Core */
 	require_once locate_template('core/init.php');
 	require_once locate_template('core/components.php');
 	require_once locate_template('core/assets-loader.php');
 	require_once locate_template('core/theme-wrapper.php');
+
+	/* Functions */
+	require_once locate_template('core/functions/seo-url.php');
 
 	/* Post Types */
 	require_once locate_template('core/post-types/cpt-produto.php');
